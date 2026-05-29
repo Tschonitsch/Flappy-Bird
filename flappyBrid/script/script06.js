3bd2bb60bb6948a40a10b1fe093cbf9358fc9e7f
Tschonitsch <brotscho@gmail.com>
2026-05-29 14:39:49 +0200

    Update script06.js

--- flappyBrid/script/script06.js
+++ flappyBrid/script/script06.js
@@ -1,13 +1,19 @@
 window.addEventListener("load", function () {
-    let startTime = Date.now(); /*Returns the number of milliseconds elapsed since midnight, January 1, 1970 Universal Coordinated Time (UTC). */
+    let startTime = Date.now(); 
+    /*Returns the number of milliseconds elapsed since midnight, January 1, 1970 Universal Coordinated Time (UTC). */
     setInterval(function () {
         let now = Date.now();
+        /* gibt die aktuelle Zeit in Millisekunden an */
+        /* bsp: startTime = 1716970000000 */
+
         let seconds = Math.floor((now - startTime) / 1000);
         /*Returns the greatest integer less than or equal to its numeric argument. */
         console.log("Seite läuft seit:", seconds, "Sekunden");
+        /* gibt sekunden in console aus */
 
         let points = document.getElementById("points");
         seconds = seconds * 1;
         points.innerText = "Points: " + seconds;
+        /* setzt die points = sekunden und ändert den text */
     });
 });
