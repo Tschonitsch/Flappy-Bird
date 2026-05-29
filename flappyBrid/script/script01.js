35a8023e8f73be865f1dc301f94ee9327b1def69
Tschonitsch <brotscho@gmail.com>
2026-05-29 14:39:21 +0200

    Update script01.js

--- flappyBrid/script/script01.js
+++ flappyBrid/script/script01.js
@@ -3,18 +3,21 @@ let bird = document.getElementById("bird");
 let y = 500;
 
 document.addEventListener("keydown", function () {
-    y = y - 100;
+    y = y - 100; /* wie viel bird nach oben geht */
 
     bird.style.top = y + "px";
-    bird.style.transform = "rotate(-20deg)";
+    bird.style.transform = "rotate(-20deg)"; /* bird rotate */
     setTimeout(reset, 500);
+    /* reset nach 500ms */
 });
+
 setInterval(function () {
     y = y + 1;
     bird.style.top = y + "px";
 });
+/* Vogel nach unten */
 
 function reset() {
     bird.style.transform = "rotate(0deg)";
     bird.style.top = y + "px";
-}
+/* Vogel reset */}
